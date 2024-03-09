763060ae516bba7e083e32f253a294d73d2e734e
AdrianWoo <747218766@qq.com>
2024-03-09 10:58:52 +0800

    Add js alert pic

--- HTMLBasic/script/main.js
+++ HTMLBasic/script/main.js
@@ -1,2 +1,10 @@
-let myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
+let myImage = document.querySelector("img");
+
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute("src");
+    if (mySrc === "./images/Firefox_logo,_2019.png"){
+        myImage.setAttribute("src","./images/firefox2.webp");
+    }else{
+        myImage.setAttribute("src","./images/Firefox_logo,_2019.png");
+    }
+};
