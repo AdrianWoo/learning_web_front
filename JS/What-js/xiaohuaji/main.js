21fd59c5125d493691859269487b3b811ade4f58
AdrianWoo <747218766@qq.com>
2024-07-02 16:53:19 +0800

    完善js

--- JS/What-js/xiaohuaji/main.js
+++ JS/What-js/xiaohuaji/main.js
@@ -3,9 +3,10 @@ const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
 const story = document.querySelector('.story');
 
-const arrayName = ['怪兽威利','大老爹','圣诞老人'];
-const arrayLocation = ['肯德基','迪士尼乐园','白宫'];
-const arrayaction = ['自燃了','在人行道化成了一坨泥','变成一条鼻涕虫爬走了'];
+const insertA = ['怪兽威利','大老爹','圣诞老人'];
+const insertB = ['肯德基','迪士尼乐园','白宫'];
+const insertC = ['自燃了','在人行道化成了一坨泥','变成一条鼻涕虫爬走了'];
+let storyText = "今天气温 34 摄氏度，:inserta:出去遛弯。当走到:insertb:门前时，突然就:insertc:。人们都惊呆了，李雷全程目睹但并没有慌，因为:inserta:是一个 130 公斤的胖子，天气又辣么热。";
 
 function randomValueFromArray(array) {
   return array[Math.floor(Math.random() * array.length)];
@@ -14,21 +15,30 @@ function randomValueFromArray(array) {
 randomize.addEventListener('click', result);
 
 function result() {
-    let name = randomValueFromArray(arrayName);
-    let location1 = randomValueFromArray(arrayLocation);
-    let action = randomValueFromArray(arrayaction);
+    let newStory = storyText;
+
+    let aItem = randomValueFromArray(insertA);
+    let bItem = randomValueFromArray(insertB);
+    let cItem = randomValueFromArray(insertC);
+    newStory = newStory.replace(':inserta:',aItem);
+    newStory = newStory.replace(':inserta:',aItem);
+    newStory = newStory.replace(':insertb:',bItem);
+    newStory = newStory.replace(':insertc:',cItem);
+
     let weight = Math.round(200) + " 公斤 ";
     let temperature = Math.round(40) + " 摄氏度";
 
   if(customName.value !== '') {
     let name = customName.value;
+    newStory = newStory.replace('李雷',name);
   }
   if(document.getElementById("american").checked) {
-     weight = Math.round(300) + "华氏度";
-     temperature = Math.round(94) +"磅";
+    const weight = Math.round(300) + "华氏度";
+    const temperature = Math.round(94) +"磅";
+    newStory = newStory.replace('34 摄氏度',weight);
+    newStory = newStory.replace('130 公斤',temperature);
   }
 
-  story.textContent = `今天气温 ${weight} ，${name}出去遛弯。当走到${location1}门前时，突然就:${action}。
-  人们都惊呆了，${customName.value}全程目睹但并没有慌，因为${name}是一个 ${temperature}的胖子，天气又辣么热。`;
+  story.textContent = newStory;
   story.style.visibility = 'visible';
 }
